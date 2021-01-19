9623adb921a3c903d91956e547b27f44d9537b98
Daria-is666 <shevchenko.darina82@gmail.com>
2021-01-20 02:38:16 +0700

    price

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// external js: flickity.pkgd.js
+// var elem = document.querySelector('.gallery');
+// var flkty = new Flickity( elem, {
+//     cellAlign: 'left',
+//         contain: true,
+//         autoPlay: 10000,
+//         freeScroll: true,
+//         wrapAround: true,
+// });
+
+// $('.gallery').flickity({
+//     // options
+//     cellAlign: 'left',
+//     contain: true,
+//     autoPlay: 10000,
+//     freeScroll: true,
+//     wrapAround: true,
+// });
+
+// alert("Javacript подключен");
